e42cd35353ff9ce300a33831f1b15c9e557570bc
manickyoj <hotshot354@gmail.com>
2015-11-17 13:43:55 -0500

    Added file instructions for warehousing script.

--- warehouse.py
+++ warehouse.py
@@ -1,3 +1,55 @@
+"""
+warehouse.py
+------------
+Nick & Filippos
+
+Purpose:
+This script uses the TABLE_CONFIG to convert rows of
+operational data from the sourceDB/sourceTable into
+amalgated rows of a destDB/destTable.
+
+Motivation:
+Querying the ~2,500,000 rows of operational data in a single year is a
+non-starter for a visualization. By grouping the data, for example by
+Year, Education, and Cause of Death, this script contains all of the data
+we need to construct a number of visualzations and only contains approximately
+720 rows of digested per year.
+
+Usage:
+The data will be grouped based on the selections array. The year is
+automatically added for each new row, as is the number of entries that
+match that row's criteria.
+
+For example, if the selections are "Education" and "Cause_Recode_39",
+it will generate a new table that looks like this.
+
+ Year | Num_in_Group | Education | Cause_Recode_39
+--------------------------------------------------
+ 2013 | 1234         | 00        | 00
+ 2013 | 1500         | 00        | 01
+ 2013 | 39           | 01        | 00
+ 2013 | 90           | 01        | 01
+
+ Num_in_Group then represents that, EG, 1234 people with educational
+ attaiment 00 (refer to the data user guide) and death cause 00
+ died in the year 2013.
+
+ The parameters are as follows:
+    - year (int): the year to attach to the data being converted
+    - dropPrevious (bool): if the destination table already exists, should
+        it be dropped before inserting the new data
+    - verbose (bool): should the script print status updates
+    - sourceDB (str): from which file should data be drawn
+    - sourceTable (str): from which table in the sourceDB should data be drawn
+    - destDB (str): into which database should data be inserted (it will be
+        created if it does not yet exist)
+    - destTable (str): into which table in the destDB should data be inserted
+        (it will be created if it does not yet exist)
+    - selections (list of strs): What columns of the source should be read,
+        grouped, and output into the destination database
+
+"""
+
 import sqlite3
 import copy
 
