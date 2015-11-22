5ab370303d7d1a38ee70cee74ad98f1e3819cc9d
manickyoj <hotshot354@gmail.com>
2015-11-22 01:46:34 -0500

    Viz nearly complete, but still needs significant touchup.

--- js/visualizer.js
+++ js/visualizer.js
@@ -11,38 +11,32 @@ function run () {
   //   updateView(data)
   // });
 
-  getData("/EducationAndCause39", (data) => {
-    const totals = {};
-    let key;
-    let eduKey;
-    let causeKey;
+  getData("/EducationAndCause39", (d) => {
+    const eduCauseData = new Data(d);
 
-    data = data.filter((datum) => {
-      // Pull the values out of the object
-      eduKey = datum["Education"]
-      causeKey = datum["Cause of Death"]
-
-      // Filter out unspecified values
-      if (
-        eduKey === "Not Specified" ||
-        causeKey === "Not Specified" ||
-        datum["Year"] !== 2008
-      ) return false;
-
-      // Sum up totals in education
-      if (eduKey in totals) totals[eduKey] += datum["Number in Group"];
-      else totals[eduKey] = datum["Number in Group"];
-
-      return true;
+    // Group specific cause of death into categories
+    // EG. All malignant neoplasm deaths -> Cancer
+    eduCauseData.addPipe((data) => {
+      return relabelData(data, "Cause of Death", GROUPINGS["Cause of Death"]);
     });
 
-    data.forEach((datum) => {
-      eduKey = datum["Education"]
-      datum["Number in Group"] = datum["Number in Group"] / totals[eduKey];
-    })
+    // Filter out all Not Specified values (for any key)
+    eduCauseData.addPipe((data) => {
+      return _.reject(data, datum => _.contains(datum, "Not Specified"));
+    });
 
-    const eduAndCause = new Viz(data, "#eduCauseViz");
-    eduAndCause.bubbleView("Cause of Death", "Education");
+    // Construct a matrix from the data
+    const eduCauseMatrix = new Matrix(
+      eduCauseData.runPipeline(),
+      "Education",
+      "Cause of Death"
+    );
+
+    // Normalize matrix by educational attainment
+    eduCauseMatrix.normalizeByRow();
+
+    // Construct a view for the matrix
+    eduCauseMatrix.bubbleView("#eduCauseViz");
   });
 }
 
@@ -311,53 +305,245 @@ function getDataRows (data, parameter, value) {
   })
 }
 
-function Viz(data, selector) {
-  this.data = data;
-
-  this.root = d3.select(selector);
-
-  this.width = parseInt(this.root.style("width"));
-  this.height = parseInt(this.root.style("height"));
+function Data(rawData) {
+  this.rawData = rawData;
+  this.pipeline = [];
 }
 
-Viz.prototype.bubbleView = function (xAxis, yAxis) {
-  const xLabels = _.uniq(_.pluck(this.data, xAxis));
-  const yLabels = _.uniq(_.pluck(this.data, yAxis));
+Data.prototype.addPipe = function(pipe) {
+  this.pipeline.push(pipe);
+}
 
-  const elemWidth = this.width / xLabels.length;
-  const elemHeight = this.height / yLabels.length;
+Data.prototype.runPipeline = function () {
+  let transformedData = this.rawData;
 
+  this.pipeline.forEach((pipe) => {
+    transformedData = pipe(transformedData);
+  });
+
+  return transformedData;
+}
+
+const GROUPINGS = {
+  "Cause of Death": {
+    "Cancer": [
+      "Malignant Neoplasm Unspecified",
+      "Malignant Neoplasm of Stomach",
+      "Malignant Neoplasm of Colon or Rectum or Anus",
+      "Malignant Neoplasm of Pancreas",
+      "Malignant Neoplasm of Trachea or Bronchus or Lung",
+      "Malignant Neoplasm of Breast",
+      "Malignant Neoplasm of Cervix or Uteri or Corpus Uteri or Ovary",
+      "Malignant Neoplasm of Prostate",
+      "Malignant Neoplasm of Urinary Tract",
+      "Non Hodgkins Lymphoma",
+      "Leukemia",
+      "Other Malignant Neoplasms",
+    ],
+
+    "Cardiovascular Disease": [
+      "Diseases of Heart",
+      "Hypertensive Heart Disease with or without Renal Disease",
+      "Ischemic Heart Diseases",
+      "Other Diseases of Heart",
+      "Essential Primary Hypertension or Hypertensive Renal Disease",
+      "Cerebrovascular Diseases",
+      "Atherosclerosis",
+      "Other Diseases of Circulatory System",
+    ],
+  },
+}
+
+function relabelData(data, key, grouping={}) {
+  return data.map((datum) => {
+    _.each(grouping, (membersOfGroup, groupName) => {
+      if (membersOfGroup.indexOf(datum[key]) != -1) datum[key] = groupName;
+    })
+
+    return datum;
+  });
+}
+
+
+
+/**
+ * Matrix
+ * ------
+ *
+ * A 'class' for organizing data into a 2D format for visualization.
+ *
+ * The Matrix format takes an array of SQL row-like data and condenses
+ * it based on two properties - one for rows and one for columns.
+ */
+
+
+/**
+ * Condense all of the data by two properties, the rowAxisKey
+ * and colAxisKey. Construct a matrix out of the results
+ * 
+ * @param  {Array} data The SQL-row-style data
+ * @param  {String} rowAxisKey The key by which to group data by row
+ * @param  {String} colAxisKey The key by which to group data by column
+ */
+function Matrix (data, rowAxisKey, colAxisKey) {
+  this.rowLabels = _.uniq(_.pluck(data, rowAxisKey));
+  this.colLabels = _.uniq(_.pluck(data, colAxisKey));
+
+  // Create a 2D Matrix with the axes specified. At each
+  // index, store an object with all of the data that match
+  // those two axis and the sum of the number of people that
+  // match that axis
+  this.matrix = new Array(this.rowLabels.length);
+  for (let row = 0; row < this.rowLabels.length; row++) {
+    this.matrix[row] = new Array(this.rowLabels.length);
+    this.colLabels.forEach((label, col) => {
+      this.matrix[row][col] = {
+        row,
+        col,
+        rowLabel: this.rowLabels[row],
+        colLabel: this.colLabels[col],
+        size: 0,
+        data: [],
+      }
+    })
+  }
+
+  // Assign all of the data to the points at which
+  // they belong
+  let row, col;
+  data.forEach((datum) => {
+    row = this.rowLabels.indexOf(datum[rowAxisKey]);
+    col = this.colLabels.indexOf(datum[colAxisKey]);
+
+    this.matrix[row][col].data.push(datum);
+    this.matrix[row][col].size += datum["Number in Group"];
+  });
+}
+
+/**
+ * Normalize the 'size' attribute of each element in the matrix
+ * so that it represents a ratio of the total data in its column.
+ */
+Matrix.prototype.normalizeByColumn = function () {
+  for (let col = 0; col < this.colLabels.length; col++ ) {
+    let colTotal = 0;
+
+    // Total column
+    for (let row = 0; row < this.rowLabels.length; row++ ) {
+      this.matrix[row][col].size = this.totalData(row, col);
+      colTotal += this.matrix[row][col].size;
+    }
+
+    // Normalize each 'size' value in column by total
+    for (let row = 0; row < this.rowLabels.length; row++) {
+      this.matrix[row][col].size /= colTotal;
+    }
+  }
+}
+
+/**
+ * Normalize the 'size' attribute of each element in the matrix
+ * so that it represents a ratio of the total data in its row.
+ */
+Matrix.prototype.normalizeByRow = function () {
+  for (let row = 0; row < this.rowLabels.length; row++ ) {
+    let rowTotal = 0;
+
+    // Total column
+    for (let col = 0; col < this.colLabels.length; col++ ) {
+      this.matrix[row][col].size = this.totalData(row, col);
+      rowTotal += this.matrix[row][col].size;
+    }
+
+    // Normalize each 'size' value in column by total
+    for (let col = 0; col < this.colLabels.length; col++) {
+      this.matrix[row][col].size /= rowTotal;
+    }
+  }
+}
+
+/**
+ * Total all of the data elements at a certain point in the matrix.
+ * 
+ * @param  {Number} row The row index of the point to total
+ * @param  {Number} col The column index of the point to total
+ * @return {Number}     The number of people represented by that matrix point
+ */
+Matrix.prototype.totalData = function (row, col) {
+  let total = 0;
+
+  this.matrix[row][col].data.forEach((datum) => {
+    total += datum["Number in Group"];
+  })
+
+  return total;
+}
+
+/**
+ * Project a view of the matrix into an SVG.
+ * 
+ * @param  {String} selector A d3 selector referring to an svg
+ */
+Matrix.prototype.bubbleView = function (selector) {
+  // Define View Variables
+  const root = d3.select(selector);
+  const width = parseInt(root.style("width"));
+  const height = parseInt(root.style("height"));
+
+  const MARGIN_INDICIES = 5;
+  const elemWidth = width / (this.colLabels.length + MARGIN_INDICIES);
+  const elemHeight = height / (this.rowLabels.length + MARGIN_INDICIES);
   const maxRadius = Math.min(elemWidth, elemHeight);
 
-  xLabels.forEach((label, index) => {
-    const pos = index * elemWidth + 0.5 * elemWidth;
-    this.root.append("text")
-      .attr({
-        x: pos,
-        y: this.height,
-        "text-anchor": "middle",
-      })
+  // Position and Create Row Labels
+  const rowLabels = root.append("g");
+  this.rowLabels.forEach((label, index) => {
+    let x = 10;
+    let y = elemHeight * (MARGIN_INDICIES + index);
+
+    rowLabels.append("text")
       .text(label)
+      .attr({
+        x,
+        y,
+        
+      })
+      .style({
+        width: `${elemWidth * (MARGIN_INDICIES)}px`,
+        "text-overflow": "ellipsis",
+      })
   });
 
-  yLabels.forEach((label, index) => {
-    const pos = index * elemHeight + 0.5 * elemHeight;
-    this.root.append("text")
-      .attr({
-        y: pos,
-      })
+  // Position and Create Column Labels
+  const colLabels = root.append("g");
+  this.colLabels.forEach((label, index) => {
+    let x = elemWidth * (MARGIN_INDICIES + index);
+    let y = elemHeight * (MARGIN_INDICIES) - 20;
+
+    colLabels.append("text")
       .text(label)
+      .attr({
+        x,
+        y,
+        "transform": `rotate(-30 ${x} ${y})`,
+      });
   });
 
-  this.root
-    .selectAll("circle")
-    .data(this.data)
-    .enter()
-    .append("circle")
-    .attr({
-      cx: d => maxRadius * xLabels.indexOf(d[xAxis]) + 0.5 * maxRadius,
-      cy: d => elemHeight * yLabels.indexOf(d[yAxis]) +  0.5 * elemHeight,
-      r: d => Math.sqrt(d["Number in Group"] / Math.PI) * maxRadius * 3,
-      "fill-opacity": 0.5,
-    })
+  // Render Data as Circles
+  const circles = root.append("g");
+  this.rowLabels.forEach((r, rowIndex) => {
+    this.colLabels.forEach((c, colIndex) => {
+      circles.append("circle")
+        .attr({
+          cx: elemWidth * (colIndex + MARGIN_INDICIES),
+          cy: elemHeight * (rowIndex + MARGIN_INDICIES),
+          r: Math.sqrt(this.matrix[rowIndex][colIndex].size / Math.PI) * maxRadius,
+          "fill-opacity": 0.9,
+          "stroke": "1px",
+        })
+        .on("mouseover", (event) => {
+          console.log(this.matrix[rowIndex][colIndex])
+        });
+    });
+  });
 }
